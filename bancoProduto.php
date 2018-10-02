2ea6489af9f76b76e9db75b7455f8197d697c2dd
CharlesNeres <charlesbritoneresjr@gmail.com>
2018-10-02 17:59:18 -0300

    iniciada o início da listagem de categorias

--- bancoProduto.php
+++ bancoProduto.php
@@ -1,13 +1,13 @@
 <?php
 
-function insereProduto($conexao, $nome, $preco, $descricao){
-    $query = "insert into produtos (nome, preco, descricao) values ('{$nome}', {$preco}, '{$descricao}')";        
+function insereProduto($conexao, $nome, $preco, $descricao, $categoria_id){
+    $query = "insert into produtos (nome, preco, descricao, categoria_id) values ('{$nome}', {$preco}, '{$descricao}', {$categoria_id})";        
     return mysqli_query($conexao, $query);
 } 
 
 function listaProdutos($conexao){
     $produtos = array();
-    $resultado = mysqli_query($conexao, "select * from produtos");
+    $resultado = mysqli_query($conexao, "select p.*, c.nome as categoria_nome from produtos as p join categorias as c on c.id = p.categoria_id");
     //mandar pegar a busca associada a esse resultado
 
     while($produto = mysqli_fetch_assoc($resultado)){//poderia fazer assim e exibir mas preciso de um loop         
--- produtoFormulario.php
+++ produtoFormulario.php
@@ -1,4 +1,9 @@
-<?php include ("cabecalho.php"); ?>
+<?php include ("cabecalho.php"); 
+include ("conecta.php");
+include ("bancoCategoria.php");
+
+$categorias = listaCategorias($conexao);
+?>
             <h1>Formulario de cadastro</h1>
             <form action="adicionaProduto.php" method="POST">
             <table>
@@ -14,6 +19,15 @@
                     <td>Descrição</td>
                     <td><textarea class="form-control" name="descricao"></textarea></td>
                 </tr>   
+                <tr>
+                    <td>Categoria</td>
+                    <td>
+                        <?php foreach($categorias as $categoria):?>
+                            <input type="radio" name="categoria_id" value="<?=$categoria['id']?>"> <?=$categoria['nome']?><br/>                            
+                        <?php endforeach ?>
+                        
+                    </td>
+                </tr>
                 <tr>
                     <td><input class="btn btn-primary" type="submit" value="Cadastrar"></td>
                 </tr>
